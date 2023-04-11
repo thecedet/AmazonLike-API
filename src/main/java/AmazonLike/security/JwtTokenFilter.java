c3ec30851ee625649348e893860f71d3d1c63735
thecedet <thecedet@live.fr>
2023-04-11 10:33:40 +0200

    bugfix jwt auth error

--- src/main/java/AmazonLike/security/JwtTokenFilter.java
+++ src/main/java/AmazonLike/security/JwtTokenFilter.java
@@ -7,12 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import AmazonLike.exception.CustomException;
 
 
 public class JwtTokenFilter extends OncePerRequestFilter {
--- src/main/java/AmazonLike/security/JwtTokenProvider.java
+++ src/main/java/AmazonLike/security/JwtTokenProvider.java
@@ -11,13 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import AmazonLike.exception.CustomException;
 import AmazonLike.model.Role;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
